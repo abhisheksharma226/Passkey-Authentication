71889229dd64aac9dfe85b3bd9528762584ae0a9
abhisheksharma226 <avisheksharma2004@gmail.com>
2024-07-18 00:29:31 +0530

    All Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { generateRegistrationOptions, verifyRegistrationResponse } = require('@simplewebauthn/server')
+const { generateRegistrationOptions, verifyRegistrationResponse , generateAuthenticationOptions, verifyAuthenticationResponse } = require('@simplewebauthn/server')
 const crypto = require('node:crypto')
 
 if (!globalThis.crypto) {
@@ -79,6 +79,44 @@ app.post('/register-verify' , async(req , res) => {
     return res.json({ verified : true })
 
 })
+
+
+app.post('/login-challenge' , async(req , res) => {
+    const { userId } = req.body;
+    if (!userStore[userId]) return res.status(404).json({ error: 'user not found' })
+
+    const opts = await generateAuthenticationOptions({
+        rpID: 'localhost',
+    })
+
+    challengeStore[userId] = opts.challenge
+
+    return res.json({ options: opts })
+
+})
+
+
+app.post('/login-verify', async (req, res) => {
+    const { userId, cred }  = req.body
+
+    if (!userStore[userId]) return res.status(404).json({ error: 'user not found!' })
+    const user = userStore[userId]
+    const challenge = challengeStore[userId]
+
+    const result = await verifyAuthenticationResponse({
+        expectedChallenge: challenge,
+        expectedOrigin: 'http://localhost:3000',
+        expectedRPID: 'localhost',
+        response: cred,
+        authenticator: user.passkey
+    })
+
+    if (!result.verified) return res.json({ error: 'something went wrong' })
+    
+    // Login the user: Session, Cookies, JWT
+    return res.json({ success: true, userId })
+})
+
 
 
 
